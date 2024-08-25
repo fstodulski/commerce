d17212011ab8589d0070c0c673627ad4547baf22
franciszekstodulski <franek.stodulski@gmail.com>
2024-08-25 19:53:59 +0200

    refactor(page.tsx): clean up imports and remove unused code to improve code readability and maintainability feat(page.tsx): add links to Media Component and Assessing Design pages for better navigation and user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,55 @@
 "use client";
 
+import { Container, Main, Section } from "@/lib/ui/craft";
+
+// React and Next.js imports
+import Link from "next/link";
+
+import { Camera, Eye } from "lucide-react";
+// Third-party library imports
+import Balancer from "react-wrap-balancer";
+
+// Local component imports
 import { Button } from "@/components/ui/button";
 import { Media } from "@/lib/media";
-import { Pause, Play } from "lucide-react";
-import { useState } from "react";
-
-const pic1 =
-  "https://images.unsplash.com/photo-1712510795837-683b93b2b95e?q=80&w=4096&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-
-const bunny =
-  "https://images.unsplash.com/photo-1452857297128-d9c29adba80b?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-
-const imageHoverCaption = () => {
-  return (
-    <span className="mt-auto font-bold text-sm text-white">Text on hover</span>
-  );
-};
-
-const videoHoverCaption = () => {
-  return (
-    <span className="mt-auto font-bold text-sm text-white">Text on hover</span>
-  );
-};
 
 export default function Home() {
-  const [isPlaying, setIsPlaying] = useState(true);
-
   return (
-    <main className="container grid grid-cols-1 gap-4 pt-20 sm:grid-cols-2 md:grid-cols-4">
-      <Media.Root className="w-40">
-        <Media.Image
-          hoverCaption={imageHoverCaption()}
-          className=""
-          width={160}
-          height={160}
-          src={pic1}
-          alt="Picture 1"
-        />
-      </Media.Root>
+    <Main>
+      <Section>
+        <Container className="flex flex-col items-center space-y-5 text-center">
+          <Media.Root className="not-prose w-full overflow-hidden rounded-lg border">
+            <Media.Image
+              alt="placeholder"
+              className=" mb-6 h-full object-cover md:mb-8 dark:invert"
+              width={1920}
+              height={1200}
+              src="https://cdn.sanity.io/images/jtds2dzp/production/41acb82d21490bcd1cd6cc4b6ccbd9d986684d54-8192x5461.jpg?rect=20,0,8152,5461&w=2560&h=1715&q=75&auto=format"
+            />
+          </Media.Root>
 
-      <Media.Root className="w-[250px]">
-        <Media.Video
-          thumbnail={{
-            src: bunny,
-            alt: "Picture 1",
-            width: 160,
-            height: 160,
-          }}
-          autoPlay={isPlaying}
-          className=""
-          onPlay={() => setIsPlaying(true)}
-          onPause={() => setIsPlaying(false)}
-          onEnded={() => console.log("ended")}
-          hoverCaption={videoHoverCaption()}
-        >
-          {(ref) => (
-            <Button
-              size="icon"
-              type="button"
-              onClick={() =>
-                isPlaying ? ref.current?.pause() : ref.current?.play()
-              }
-              className="absolute top-2 right-2 z-20 opacity-0 duration-300 ease-in-out group-hover:opacity-100"
-            >
-              {isPlaying ? <Pause size={16} /> : <Play size={16} />}
+          <h1 className="!mb-0">
+            <Balancer>Hello there!</Balancer>
+          </h1>
+          <h3 className="text-muted-foreground">
+            <Balancer>Check out my solution for:</Balancer>
+          </h3>
+          <div className="not-prose mt-6 flex gap-2 md:mt-12">
+            <Button asChild>
+              <Link href="/media-component">
+                <Camera className="mr-2" />
+                Media Component
+              </Link>
             </Button>
-          )}
-        </Media.Video>
-      </Media.Root>
-    </main>
+            <Button variant={"ghost"} asChild>
+              <Link href="/assessing-design">
+                <Eye className="mr-2" />
+                Assessing Design
+              </Link>
+            </Button>
+          </div>
+        </Container>
+      </Section>
+    </Main>
   );
 }
